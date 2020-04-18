299f11c345c366f3ddf6d0597136ec3e95194e2a
Egbedokun Olayinka <egbedokunolayinka@gmail.com>
2020-04-18 22:34:06 +0100

    js task 3 Start.NG

---
+++ task3.js
@@ -0,0 +1,27 @@
+let yugioh = function(num) {
+    let arr = [];
+    for(let i=1;i<=num;i++) {
+        if (i%2===0 && i%3===0 && i%5===0) {
+            arr.push('yu-gi-oh');
+        } else if (i%2===0 && i%5===0) {
+            arr.push('yu-oh');
+        } else if (i%3===0 && i%5===0) {
+            arr.push('gi-oh');
+        } else if (i%2===0 && i%3===0) {
+            arr.push('yu-gi');
+        } else if (i%2===0) {
+            arr.push('yu');
+        } else if (i%3===0) {
+            arr.push('gi');
+        } else if (i%5===0) {
+            arr.push('oh');
+        } else {
+            arr.push(i);
+        }
+        
+    }
+    console.log(arr);
+}
+
+yugioh(100);
+yugioh(30);
